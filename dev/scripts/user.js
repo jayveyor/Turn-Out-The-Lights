04c813edfe6e094ff2e2981cb099f31f0eac8ff4
jayveyor <35696896+jayveyor@users.noreply.github.com>
2018-02-27 14:10:16 -0500

    some final styles

--- dev/scripts/user.js
+++ dev/scripts/user.js
@@ -6,21 +6,24 @@ const User = (props) => {
         <div className="user">
 
         <div className="wrap">
-        <div className="flex">
+        
                     <div className="grid">  <h2 className="characterName">{props.characterName}</h2>
                     {/* <button onClick={props.Mute}>Mute</button> */}
-                    <h2 className="health"> {props.health}/5</h2><img src="heart.gif" alt="heart" /></div>   
+                    <h2 className="health"> {props.health}/5</h2><img src="heart.gif" alt="heart" />   
                        
 
                     <div className="flashlight" onClick={props.clickFlashlight}> {props.flashlightImg} 
                     
                         {props.batteryLife}
                     </div>
-                    <div className="batteries"><h2>{`X ${props.battery}`}</h2><img src="battery.png" alt="battery" /></div>
+                        <div className="batteries"><h2>{`X ${props.battery}`}</h2><img src="battery.png" alt="battery" /></div>
+                        
+    
+         </div>
         </div>
         </div>
 
-        </div>
+        
     )
 }
 
